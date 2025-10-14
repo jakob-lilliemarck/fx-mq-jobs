0e9cda569d44b319266d6d019e5f631eb102ffbe
jakob <lilliemarckjakob@gmail.com>
2025-10-14 21:55:03 +0200

    fix: expose PublishError publicly

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,11 @@ mod listener;
 mod publisher;
 
 pub use constants::FX_MQ_JOBS_SCHEMA_NAME;
+pub use fx_mq_building_blocks::migrator::*;
 pub use handler::{Handler, RegistryBuilder, RegistryError};
 pub use lease_renewer::{LeaseRenewalError, LeaseRenewer};
 pub use listener::listener::Listener;
-pub use publisher::Publisher;
+pub use publisher::{PublishError, Publisher};
 
 #[cfg(test)]
 pub mod test_tools;
